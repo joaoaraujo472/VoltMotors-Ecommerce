ef49be6213d0c72a955d98100d8fa52a0b8a02c3
João <joaoaaraujosilva2022@gmail.com>
2026-05-24 18:49:42 -0300

    feat: editando a imgem do sobre.html e alterando texto

--- src/js/script.js
+++ src/js/script.js
@@ -87,5 +87,94 @@ function renderizarProdutos() {
   });
 }
 
-// CHAMAR FUNÇÃO
-renderizarProdutos();
+// ================================
+// CARRINHO
+// ================================
+
+const carrinho = [
+  {
+    nome: "VOLT CITY",
+    preco: 12990.90
+  },
+
+  {
+    nome: "VOLT SPORT",
+    preco: 18990.00
+  },
+
+  {
+    nome: "VOLT TRAIL",
+    preco: 21900.00
+  }
+];
+
+// RENDERIZAR CARRINHO
+
+function renderizarCarrinho() {
+
+  const listaCarrinho = document.getElementById("lista-carrinho");
+
+  if (!listaCarrinho) return;
+
+  listaCarrinho.innerHTML = "";
+
+  carrinho.forEach(item => {
+
+    const produto = document.createElement("div");
+
+    produto.classList.add("item-carrinho");
+
+    produto.innerHTML = `
+      <h3>${item.nome}</h3>
+      <p>${formatarMoeda(item.preco)}</p>
+    `;
+
+    listaCarrinho.appendChild(produto);
+
+  });
+
+  atualizarTotal();
+
+}
+
+// TOTAL COM REDUCE
+
+function atualizarTotal() {
+
+  const totalElemento = document.getElementById("total-compra");
+
+  if (!totalElemento) return;
+
+  const total = carrinho.reduce((acumulador, item) => {
+    return acumulador + item.preco;
+  }, 0);
+
+  totalElemento.textContent = formatarMoeda(total);
+
+}
+
+// DESCONTO
+
+const btnDesconto = document.getElementById("btn-desconto");
+
+if (btnDesconto) {
+
+  btnDesconto.addEventListener("click", () => {
+
+    const totalElemento = document.getElementById("total-compra");
+
+    const total = carrinho.reduce((acumulador, item) => {
+      return acumulador + item.preco;
+    }, 0);
+
+    const totalComDesconto = total * 0.9;
+
+    totalElemento.textContent =
+      formatarMoeda(totalComDesconto);
+
+  });
+
+}
+
+// CHAMAR
+renderizarCarrinho();
